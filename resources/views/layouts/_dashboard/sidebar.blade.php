7f3a0fa932f075986295ecde70f8c3366a71560c
Guido Famula <guidofamula29@gmail.com>
2022-11-03 00:16:48 +0700

    adding active status for categories sidebar if views edit category clicked/visited

--- resources/views/layouts/_dashboard/sidebar.blade.php
+++ resources/views/layouts/_dashboard/sidebar.blade.php
@@ -21,7 +21,7 @@
                 {{ trans('dashboard.link.posts') }}
             </a>
             {{-- Menu Categories --}}
-            <a class="nav-link {{ set_active(['categories.index', 'categories.create']) }}" href="{{ route('categories.index') }}">
+            <a class="nav-link {{ set_active(['categories.index', 'categories.create', 'categories.edit']) }}" href="{{ route('categories.index') }}">
                 <div class="sb-nav-link-icon">
                     <i class="fas fa-bookmark"></i>
                 </div>
