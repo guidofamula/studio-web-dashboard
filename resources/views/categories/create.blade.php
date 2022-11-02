8dd5b72192f2676fff0132c14d88150c93d76aa0
Guido Famula <guidofamula29@gmail.com>
2022-11-02 11:20:52 +0700

    Initialize route for form action & validate error on require

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -13,20 +13,31 @@
     <div class="col-md-12">
        <div class="card">
           <div class="card-body">
-             <form action="" method="POST">
+             <form action="{{ route('categories.store') }}" method="POST">
+               @csrf
                 <!-- title -->
                 <div class="form-group">
                    <label for="input_category_title" class="font-weight-bold">
                       {{ trans('categories.form_control.input.title.label') }}
                    </label>
-                   <input id="input_category_title" value="" name="title" type="text" class="form-control" placeholder="{{ trans('categories.form_control.input.title.placeholder') }}" />
+                   <input id="input_category_title" value="" name="title" type="text" class="form-control @error('title') is-invalid @enderror" placeholder="{{ trans('categories.form_control.input.title.placeholder') }}" />
+                   @error('title')
+                      <span class="invalid-feedback" role="alert">
+                          <strong>{{ $message }}</strong>
+                      </span>
+                   @enderror
                 </div>
                 <!-- slug -->
                 <div class="form-group">
                    <label for="input_category_slug" class="font-weight-bold">
                       {{ trans('categories.form_control.input.slug.label') }}
                    </label>
-                   <input id="input_category_slug" value="" name="slug" type="text" class="form-control" placeholder="{{ trans('categories.form_control.input.slug.placeholder') }}" readonly />
+                   <input id="input_category_slug" value="" name="slug" type="text" class="form-control @error('slug') is-invalid @enderror" placeholder="{{ trans('categories.form_control.input.slug.placeholder') }}" readonly />
+                   @error('slug')
+                      <span class="invalid-feedback" role="alert">
+                          <strong>{{ $message }}</strong>
+                      </span>
+                   @enderror
                 </div>
                 <!-- thumbnail -->
                 <div class="form-group">
@@ -39,8 +50,13 @@
                             {{ trans('categories.button.browse.value') }}
                          </button>
                       </div>
-                      <input id="input_category_thumbnail" name="thumbnail" value="" type="text" class="form-control" placeholder="{{ trans('categories.form_control.input.thumbnail.placeholder') }}"
+                      <input id="input_category_thumbnail" name="thumbnail" value="" type="text" class="form-control @error('thumbnail') is-invalid @enderror" placeholder="{{ trans('categories.form_control.input.thumbnail.placeholder') }}"
                          readonly />
+                         @error('thumbnail')
+                           <span class="invalid-feedback" role="alert">
+                              <strong>{{ $message }}</strong>
+                           </span>
+                        @enderror
                    </div>
                 </div>
                 {{-- Data Preview thumbnail --}}
@@ -58,7 +74,12 @@
                    <label for="input_category_description" class="font-weight-bold">
                       {{ trans('categories.form_control.textarea.description.label') }}
                    </label>
-                   <textarea id="input_category_description" name="description" class="form-control" rows="3" placeholder="{{ trans('categories.form_control.textarea.description.placeholder') }}"></textarea>
+                   <textarea id="input_category_description" name="description" class="form-control @error('description') is-invalid @enderror" rows="3" placeholder="{{ trans('categories.form_control.textarea.description.placeholder') }}"></textarea>
+                   @error('description')
+                      <span class="invalid-feedback" role="alert">
+                          <strong>{{ $message }}</strong>
+                      </span>
+                   @enderror
                 </div>
                 <div class="float-right">
                 	<a class="btn btn-warning px-4" href="{{ route('categories.index') }}">
