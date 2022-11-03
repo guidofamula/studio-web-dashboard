b0282da19981fde4bb090c7bce0ab0daf40b0d1a
Guido Famula <guidofamula29@gmail.com>
2022-11-04 04:11:36 +0700

    add view to method show category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -119,7 +119,9 @@ class CategoryController extends Controller
      */
     public function show(Category $category)
     {
-        //
+        return view('categories.show', [
+            'category' => $category,
+        ]);
     }
 
     /**
