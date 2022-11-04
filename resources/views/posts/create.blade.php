9a916afb8c9d5cb7b06054625210612e689b0f6f
Guido Famula <guidofamula29@gmail.com>
2022-11-05 03:54:16 +0700

    Sync file manager to toolbar tinyMCE

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -162,8 +162,33 @@
 			    "emoticons template paste textpattern",
 			  ],
 			  toolbar1: "fullscreen preview",
-			  toolbar2:
-			    "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media",
+			  toolbar2: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media",
+			  file_picker_callback: function(callback, value, meta) {
+					   let x = window.innerWidth || document.documentElement.clientWidth || document
+					      .getElementsByTagName('body')[0].clientWidth;
+					   let y = window.innerHeight || document.documentElement.clientHeight || document
+					      .getElementsByTagName('body')[0].clientHeight;
+
+					   let cmsURL = "{{ route('unisharp.lfm.show') }}" + '?editor=' + meta.fieldname;
+					   if (meta.filetype == 'image') {
+					      cmsURL = cmsURL + "&type=Images";
+					   } else {
+					      cmsURL = cmsURL + "&type=Files";
+					   }
+
+					   tinyMCE.activeEditor.windowManager.openUrl({
+					      url: cmsURL,
+					      title: 'Filemanager',
+					      width: x * 0.8,
+					      height: y * 0.8,
+					      resizable: "yes",
+					      close_previous: "no",
+					      onMessage: (api, message) => {
+					         callback(message.content);
+					      }
+					   });
+					}
+
 			});
 
 		});
